83d19539e17cce7210e8a00d57b389c03a7c8dfd
Chaokai Lai <chaokai.lai@gmail.com>
2021-04-27 17:21:58 +1000

    separate front end to a new repo

--- client/src/api/api.ts
+++
@@ -1,5 +0,0 @@
-import baseUrl from '../config/apis';
-
-export const apiUserLogin = (): string => `${baseUrl}/user/login`;
-export const apiUserGoogleLogin = (): string => `${baseUrl}/user/googlelogin`;
-export const apiUserSignUp = (): string => `${baseUrl}/user/signup`;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import userRoutes from './routes/userRoutes.js';
 import { errorHandler, notFoundHandler } from './middlewares/errorMiddleware.js';
 import connectDB from './config/mongoDB.js';
 import cors from 'cors';
-import path from 'path';
 
 dotenv.config();
 connectDB();
@@ -16,7 +15,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api/user', userRoutes);
+app.use('/user', userRoutes);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
